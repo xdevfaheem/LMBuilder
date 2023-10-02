b91afbcf12262b8e626c15b9f85f5ce033992058
Mohammed Faheem <immohammedfaheem@gmail.com>
2023-10-02 22:44:29 +0530

    Create llm_config.py

---
+++ llm-builder/llm/llm_config.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTConfig:
+    
+    """
+    vocab_size : Vocab Size of the model
+    max_seq_len : Maximum Length of the Sequence/words
+    embedding_dim : Dimensionality for Embedding
+    num_blocks : Number of Decoder Blocks
+    activation : Activation to use inbetween feed forward layer. default is `relu`
+    expansion_factor : decides the projection of inbetween neurons in feed forward layer
+    num_heads : Number of Attention Heads
+    droput : percentage of layers to dropout to prevent overfitting and for a stable training. default is None
+    """
+    
+    max_seq_len=1024
+    vocab_size=8000
+    embed_dropout=0.1
+    dropout=0.1
+    bias=True
+    num_heads = 8
+    num_blocks = 4
+    expansion_factor =4
+    activation_type="relu"
+    embedding_dim = 512
+    pretrained_embeddings = None
+    freeze_embeddings = False
