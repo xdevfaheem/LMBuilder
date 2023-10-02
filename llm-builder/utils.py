f57737ac7da0867a4562956a768496a676f3d1d9
Mohammed Faheem <immohammedfaheem@gmail.com>
2023-10-02 23:18:37 +0530

    Update utils.py

--- llm-builder/utils.py
+++ llm-builder/utils.py
@@ -1,3 +1,7 @@
+from functools import lru_cache
+from importlib.util import find_spec
+import pkg_resources
+
 
 def number_to_words(number):
     
@@ -23,3 +27,79 @@ def number_to_words(number):
         return number_to_words(number // 1000000000000) + " trillion" + (" " + number_to_words(number % 1000000000000) if number % 1000000000000 != 0 else "")
     else:
         raise ValueError("Number out of range")
+
+@lru_cache()
+def package_available(package_name: str) -> bool:
+    
+    """Check if a package is available in your environment.
+    >>> package_available('os')
+    True
+    >>> package_available('bla')
+    False
+    """
+    
+    try:
+        return find_spec(package_name) is not None
+    except ModuleNotFoundError:
+        return False
+
+def is_pkg_req_met(req, msg=False):
+
+    """
+    check if the package requirement with extras and version specifiers on available in your env.
+
+    >>> is_pkg_req_met("torch>=0.1", msg=True)
+    Package with specified version critiria 'torch>=0.1' met!
+    >>> is_pkg_req_met("torch>=0.1")
+    True
+    >>> is_pkg_req_met("torch>100.0")
+    False
+    """
+
+    try:
+        pkg_resources.require(req)
+        met = True
+        message = f"Package with specified version critiria {req!r} met!"
+    except Exception as ex:
+        met = False
+        message = f"{ex.__class__.__name__}: {ex}. Package with specified version critiria not met! \n Try running `pip install -U {req!r}`"
+    
+    if msg:
+        print(messsage)
+    
+    return met
+
+class PkgRequirement:
+    """Boolean-like class for check of requirement with extras and version specifiers.
+
+    >>> RequirementCache("torch>=0.1")
+    Requirement 'torch>=0.1' met
+    >>> bool(RequirementCache("torch>=0.1"))
+    True
+    >>> bool(RequirementCache("torch>100.0"))
+    False
+    """
+
+    def __init__(self, requirement: str) -> None:
+        self.requirement = requirement
+
+    def _check_requirement(self) -> None:
+        if not hasattr(self, "available"):
+            try:
+                pkg_resources.require(self.requirement)
+                self.available = True
+                self.message = f"Requirement {self.requirement!r} met"
+            except Exception as ex:
+                self.available = False
+                self.message = f"{ex.__class__.__name__}: {ex}. HINT: Try running `pip install -U {self.requirement!r}`"
+
+    def __bool__(self) -> bool:
+        self._check_requirement()
+        return self.available
+
+    def __str__(self) -> str:
+        self._check_requirement()
+        return self.message
+
+    def __repr__(self) -> str:
+        return self.__str__()
