5924ce598ba32a28e5ed9fafd7ea547fe28bda44
Mohammed Faheem <immohammedfaheem@gmail.com>
2023-10-02 22:42:50 +0530

    Update model.py

--- llm-builder/llm/model.py
+++ llm-builder/llm/model.py
@@ -11,33 +11,6 @@ from torch.nn import functional as F
 from utils import number_to_words
 from flash_attn import flash_attn_func
 
-@dataclass
-class GPTConfig:
-    
-    """
-    target_vocab_size : Target Vocab Size for Final Projection
-    max_seq_len : Maximum Length of the Sequence/words
-    embedding_dim : Dimensionality for Embedding
-    num_blocks : Number of Decoder Blocks
-    activation : Activation to use inbetween feed forward layer. default is `relu`
-    expansion_factor : decides the projection of inbetween neurons in feed forward layer
-    num_heads : Number of Attention Heads
-    droput : percentage of layers to dropout to prevent overfitting and for a stable training. default is None
-    """
-    
-    max_seq_len=1024
-    vocab_size=8000
-    embed_dropout=0.1
-    dropout=0.1
-    bias=True
-    num_heads = 8
-    num_blocks = 4
-    expansion_factor =4
-    activation_type="relu"
-    embedding_dim = 512
-    pretrained_embeddings = None
-    freeze_embeddings = False
-
 
 # class for Embedding Layer to project the input sequences to Multi Dimensional Space
 class EmbeddingLayer(nn.Module):
