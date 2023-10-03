b6d5c90a45f634d6531e262b0ea084f5aa2083c3
Mohammed Faheem <immohammedfaheem@gmail.com>
2023-10-03 22:01:35 +0530

    Update layernorm.py

--- llm-builder/llm/layernorm.py
+++ llm-builder/llm/layernorm.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class LayerNormWithBias(nn.Module):
-    """ LayerNorm but with an optional bias. PyTorch doesn't support passing `bias` argument """
+    """ LayerNorm but with an optional bias. PyTorch doesn't support passing `bias` argument for nn.LayerNorm"""
 
     def __init__(self, ndim, bias):
         super(LayerNormWithBias).__init__()
@@ -13,27 +13,43 @@ class LayerNormWithBias(nn.Module):
 
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
+        
 
-class RMSNorm(torch.nn.Module):
-  
+class RMSNorm(nn.Module):
+
     """
-    Root Mean Square Layer Normalization.
-      
-    Derived from https://github.com/bzhangGo/rmsnorm/blob/master/rmsnorm_torch.py. BSD 3-Clause License:
-    https://github.com/bzhangGo/rmsnorm/blob/master/LICENSE.
+    Root Mean Square Layer Normalization. 
+    To reduce (avoid) Internal Covariance Shift (ICS)
+
+    RMSNorm Claims re-centering (0-mean) is the success of LayerNorm rather than re-scaling (variance-1).
+    Basically they claims, success of layernorm is not because of re-centering and re-scaling,
+    but mostly because of re-scaling (to have a variance of 1)
     """
 
-    def __init__(self, size: int, dim: int = -1, eps: float = 1e-5) -> None:
+    def __init__(
+            self,
+            normalized_shape,
+            eps=1e-5,
+            weight=True,
+            dtype=None,
+            device=None,
+    ):
         super(RMSNorm).__init__()
-        self.weight = torch.nn.Parameter(torch.ones(size))
         self.eps = eps
-        self.dim = dim
+        if weight:
+            self.weight = torch.nn.Parameter(
+                torch.ones(normalized_shape, dtype=dtype, device=device)
+            )
+        else:
+            self.register_parameter('weight', None)
 
-      def forward(self, x: torch.Tensor) -> torch.Tensor:
-          # NOTE: the original RMSNorm paper implementation is not equivalent
-          norm_x = torch.mean(x * x, dim=self.dim, keepdim=True)
-          x_normed = x * torch.rsqrt(norm_x + self.eps)
-          return self.weight * x_normed
-
-      def reset_parameters(self):
-          torch.nn.init.ones_(self.weight)
+    def rms_norm(self, x: torch.Tensor, weight=None, eps=1e-5) -> torch.Tensor:
+        # NOTE: the original RMSNorm paper implementation is not equivalent
+        norm_x = torch.mean(x * x, dim=-1, keepdim=True)
+        x_normed = x * torch.rsqrt(norm_x + eps)
+        if weight is not None:
+            return x_normed * weight
+        return x_normed
+    
+    def forward(self, x):
+        return self.rms_norm(x.float(), self.weight, self.eps).to(dtype=x.dtype)
